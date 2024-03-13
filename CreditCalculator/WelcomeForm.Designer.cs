9f222599996587fb12e561cb4ab8039060bf0490
Angelika Szczerbowska <andzia913@gmail.com>
2024-03-13 12:40:24 +0100

    Style welcome form

--- CreditCalculator/WelcomeForm.Designer.cs
+++ CreditCalculator/WelcomeForm.Designer.cs
@@ -34,21 +34,26 @@
             // 
             // btn_closeWelcomeForm
             // 
+            this.btn_closeWelcomeForm.BackColor = System.Drawing.Color.White;
+            this.btn_closeWelcomeForm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_closeWelcomeForm.Font = new System.Drawing.Font("Rubik", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btn_closeWelcomeForm.Location = new System.Drawing.Point(200, 369);
             this.btn_closeWelcomeForm.Name = "btn_closeWelcomeForm";
             this.btn_closeWelcomeForm.Size = new System.Drawing.Size(350, 42);
             this.btn_closeWelcomeForm.TabIndex = 0;
             this.btn_closeWelcomeForm.Text = "Przejdz do klakulatora";
-            this.btn_closeWelcomeForm.UseVisualStyleBackColor = true;
+            this.btn_closeWelcomeForm.UseVisualStyleBackColor = false;
             this.btn_closeWelcomeForm.Click += new System.EventHandler(this.btn_closeWelcomeForm_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rubik", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(320, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 29);
+            this.label1.Size = new System.Drawing.Size(82, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Witaj!";
             // 
@@ -56,6 +61,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CreditCalculator.Properties.Resources.black_1072366_1280;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_closeWelcomeForm);
